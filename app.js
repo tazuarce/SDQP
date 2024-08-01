27115a00b97b0545c6e296110d60c051ae0e9d6c
tazuarce <tazuarce@gmail.com>
2024-08-01 20:52:31 -0300

    más noticieros y mejora interna
    ahora no están los links y videos hardcodeados en el html, sino que son generados dinámicamente desde javascript usando una lista con canales y respectivos códigos de video

--- app.js
+++ app.js
@@ -1,31 +1,81 @@
 /* console.log('javascript empieza') */
 
-// carga de la APU de youtube
+// carga de la API de youtube
 let tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 let firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-let frames;
 let players;
-let videos = document.querySelectorAll('.video');
-function onYouTubeIframeAPIReady() {
-    /* console.log('API de YouTube cargada'); */
-    frames = document.querySelectorAll('iframe');
-    players = [];
-    for(i = 0 ; i < frames.length ; i++){
-        players.push(new YT.Player(frames[i],{events:{}}));
-    }
+let videos;
 
-    // asigno ids a los contenedores de los videos para poder usarlos después en las funciones mute, unmute y abrir
-    for(i = 0 ; i < videos.length ; i++){
-        videos[i].id = i;
-    }
+let idSonando; // indica la ID del video que está sonando en el momento; la usa para abrir el video en otra pestaña
 
+const codigosDeVideos = {
+    "TN":"cb12KmMMDJA",
+    "LN+":"ocFG3yor--M",
+    "C5N":"NdQSOItOQ5Y",
+    "Crónica":"avly0uwZzOE",
+    "el doce":"sFZe_RPnNSo",
+    "telefe noticias":"8gs-9lsfVQU",
+    /* "A24" : "QGpHLgRnrx4", */ // ;( no disponible dice
+    "Canal 26":"rY6a3fuaQ5Q",
+    "T13":"kc3JyNqbuPU",
+    "France 24":"Y-IlMeCCtIg",
+    "FOX":"YDfiTGGPYCk",
+    "CBS News": "GiIQ-aCCA_w",
+    "USA Today" : "vZYMwAm8sso",
+    "ABC7 New York" : "oFOTr81KHLg",
+    "FORO" : "Ulwgdf5t46o",
+    "euronews" : "O9mOtdZ-nSk",
+    "France 24" : "Y-IlMeCCtIg",
+    "NASA + SpaceX" : "mhJRzQsLZGg",
+    "CNA" : "XWq5kBlakcQ",
+    "GB News" : "8WX6YL9JnLw",
+    "Canal Sur Andalucía" : "P3FhO1UUGE8",
+    "ABC News" : "-mvUkiILTqI",
+    "CGTN Europe" : "UkcHAbq9PVQ",
+    "Watch Sky News" : "oJUvTVdTMyY"
 }
 
-let idSonando;
+function onYouTubeIframeAPIReady() {
+    let monitor = document.getElementById('monitor');
+    
+    let newDiv, newVideo, newItem;
+    players = [];
+    let idNumerico = 0;
+    for(let canal of Object.keys(codigosDeVideos)) {
+        newDiv = document.createElement('div');
+        newDiv.setAttribute('id', 'this');
 
+        newVideo = document.createElement('div');
+        newVideo.classList.add('video');
+        newVideo.setAttribute("onclick", "clickeado(this)")
+        newVideo.appendChild(newDiv);
+
+        newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.appendChild(newVideo);
+
+        monitor.appendChild(newItem);
+
+        players.push(new YT.Player('this',{
+            videoId: codigosDeVideos[canal],
+            playerVars: { 'autoplay': 1, 'controls': 0, 'mute': 1 , 'enablejsapi': 1},
+            events:{}
+        }));
+
+        document.getElementById('this').parentElement.setAttribute('id', idNumerico);
+        document.getElementById('this').setAttribute('id', '');
+        idNumerico++;
+    }
+
+    videos = document.querySelectorAll('.video');
+    console.log(videos);
+}
+
+
+// esto debería poder cambiarlo por algo que reinicie los videos que no están sonando
 document.addEventListener("visibilitychange", function() {
     if (document.visibilityState === "visible") {
         location.reload();
@@ -68,10 +118,10 @@ function unmute(id){
 }
 
 function abrir(id){
-    console.log(players);
+    /* console.log(players);
     console.log(id);
     console.log(players[id]);
-    console.log(players[id].getVideoUrl());
+    console.log(players[id].getVideoUrl()); */
     let link = players[id].getVideoUrl();
     window.open(link,'_blank');
 }
